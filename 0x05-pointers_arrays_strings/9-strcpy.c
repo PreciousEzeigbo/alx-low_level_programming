603f556c53a17822ba046f7cd1ffde609be13c43
“Precious <preciousezeigbo81@gmail.com>
2023-03-28 11:36:34 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,23 @@
 #include "main.h"
 /**
- * _strcpy - copies strings
- * @dest: destination array
- * @src: source array
- * Return: copied string
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int l = 0;
+	int x = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
 	return (dest);
 }

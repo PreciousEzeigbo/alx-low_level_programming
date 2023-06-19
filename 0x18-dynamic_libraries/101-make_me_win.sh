e307789c4bea3162700ab69d64a936e7752bb631
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-19 15:55:14 -0700

    Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://rawgithub.com/PreciousEzeigbo/alx-low_level_programming/master/0x18-dynamic_libraries/libtask.so
+wget -P /tmp/ https://raw.github.com/PreciousEzeigbo/alx-low_level_programming/master/0x18-dynamic_libraries/libtask.so
 export LD_PRELOAD=/tmp/libtask.so
